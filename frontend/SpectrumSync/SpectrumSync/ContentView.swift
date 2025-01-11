a2ba435b0587e2ad71a3365b26b48c1a1a3a8a9b
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-01-10 21:50:38 -0700

    fix: updated .gitignore to exclude unnecessary files

---
+++ frontend/SpectrumSync/SpectrumSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpectrumSync
+//
+//  Created by Hamidat Bello on 2025-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frontend/SpectrumSync/SpectrumSync/SpectrumSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectrumSyncApp.swift
+//  SpectrumSync
+//
+//  Created by Hamidat Bello on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct SpectrumSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
