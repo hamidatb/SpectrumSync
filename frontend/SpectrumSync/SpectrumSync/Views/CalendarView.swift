920925f1f9b99ec280db525c85501cb5f00b0c9a
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-02-28 11:55:20 -0700

    feat(cal): Add calendar ui customization

--- frontend/SpectrumSync/SpectrumSync/Views/CalendarView.swift
+++ frontend/SpectrumSync/SpectrumSync/Views/CalendarView.swift
@@ -1,5 +1,5 @@
 //
-//  CalendarView.swift
+//  CustomDayView.swift
 //  SpectrumSync
 //
 //  Created by Hamidat Bello on 2025-02-28.
@@ -9,15 +9,63 @@ import SwiftUI
 import MijickCalendarView
 
 struct CalendarView: View {
-    @State private var selectedDate: Date? = nil
-    @State private var selectedRange: MDateRange? = .init()
-
+    @State private var selectedDate: Date?
+    
     var body: some View {
-        MCalendarView(selectedDate: $selectedDate, selectedRange: $selectedRange)
+        ZStack {
+            createContent()
+        }
+    }
+}
+private extension CalendarView {
+    func createContent() -> some View {
+        VStack(spacing: 0) {
+            Spacer().frame(height: 32)
+            createCalendarView()
+        }
     }
 }
 
+private extension CalendarView {
 
+    func createCalendarView() -> some View {
+        MCalendarView(selectedDate: $selectedDate, selectedRange: nil, configBuilder: configureCalendar)
+            .padding(.horizontal, margins)
+    }
+}
+private extension CalendarView {
+    func configureCalendar(_ config: CalendarConfig) -> CalendarConfig {
+        config
+            .daysHorizontalSpacing(9)
+            .daysVerticalSpacing(19)
+            .monthsBottomPadding(16)
+            .monthsTopPadding(42)
+    }
+}
+// private extension CalendarView {
+   //  func buildDayView(_ date: Date, _ isCurrentMonth: Bool, selectedDate: Binding<Date?>?, range: Binding<MDateRange?>?) -> DV.ColoredRectangle {
+        // return .init(date: date, color: getDateColor(date), isCurrentMonth: isCurrentMonth, selectedDate: selectedDate, selectedRange: nil)
+    // }
+// }
+
+private extension CalendarView {
+    func onContinueButtonTap() { }
+    func getDateColor(_ date: Date) -> Color? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        
+        let day = Int(dateFormatter.string(from: date)) ?? 0
+        if day % 5 == 0 { return .red }
+        if day % 9 == 0 { return .orange}
+        if day % 11 == 0  { return .green }
+        return nil
+    }
+}
+
+// MARK: - Modifiers
+fileprivate let margins: CGFloat = 28
+
+// MARK: - Preview
 #Preview {
     CalendarView()
 }
